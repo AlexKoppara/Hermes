1d4d65394ca1587c96e9c62a061156035fe8b620
Alex Koppara <alexkoppara@Alexs-MBP.home>
2020-05-21 21:51:15 -0400

    init

---
+++ graph/entity.resolvers.go
@@ -0,0 +1,25 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/AlexKoppara/Hermes/graph/generated"
+	"github.com/AlexKoppara/Hermes/graph/model"
+)
+
+func (r *entityResolver) FindMenuItemByID(ctx context.Context, id string) (*model.MenuItem, error) {
+	menuItem := &model.MenuItem{
+		ID:           "menu_item_1",
+		Name:         "chicken wings",
+		PriceInCents: 1500,
+	}
+	return menuItem, nil
+}
+
+// Entity returns generated.EntityResolver implementation.
+func (r *Resolver) Entity() generated.EntityResolver { return &entityResolver{r} }
+
+type entityResolver struct{ *Resolver }
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,29 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/AlexKoppara/Hermes/graph/generated"
+	"github.com/AlexKoppara/Hermes/graph/model"
+)
+
+func (r *queryResolver) Orders(ctx context.Context) ([]*model.Order, error) {
+	var menuItems []*model.MenuItem
+	menuItems
+
+	var orders []*model.Order
+	orders = append(orders, &model.Order{
+		ID: "order_1",
+		MenuItems: ,
+	})
+	return orders, nil
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
